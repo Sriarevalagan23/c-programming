9de89b5cb5d0eea9f7c42a095e08d6f096feb81b
Srinidhi A <158122890+Sriarevalagan23@users.noreply.github.com>
2024-02-24 10:31:31 +0530

    Add files via upload

---
+++ arithmetic operations (2).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+int a=10, b=3, sum,diff,mul,div,mod;
+sum=a+b;
+diff=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+printf("%d",sum);
+printf("%d",diff);
+printf("%d",mul);
+printf("%d",div);
+printf("%d",mod);
+return 0;
+}
